7a9c256ce60ed45a6a17ee1d23233ff36436ca48
Siluet3656 <Mingaleev02@mail.ru>
2025-11-02 16:12:23 +0400

    Manu panels

---
+++ Assets/MenuSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MenuSwitcher : MonoBehaviour
+{
+    [SerializeField] private GameObject _workMenu;
+    [SerializeField] private GameObject _shopMenu;
+    [SerializeField] private GameObject _mailMenu;
+
+    private void HideAll()
+    {
+        _workMenu.SetActive(false);
+        _shopMenu.SetActive(false);
+        _mailMenu.SetActive(false);
+    }
+
+    public void ShowWork()
+    {
+        HideAll();
+        _workMenu.SetActive(true);
+    }
+    
+    public void ShowShop()
+    {
+        HideAll();
+        _shopMenu.SetActive(true);
+    }
+
+    public void ShowMail()
+    {
+        HideAll();
+        _mailMenu.SetActive(true);
+    }
+}
